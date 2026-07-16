a78e2cca7a825ad2fa5d83e835c8cab4214f2e65
Lynda O <nwaekpelynda1994@gmail.com>
2026-07-16 22:54:34 +0100

    Rename last photo label to last seen

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,8 +1,8 @@
-const SPOTIT_CACHE = "spotit-shell-v57";
+const SPOTIT_CACHE = "spotit-shell-v58";
 const SHELL_ASSETS = [
   "/",
   "/index.html",
-  "/manifest.webmanifest?v=57",
+  "/manifest.webmanifest?v=58",
   "/assets/brand/spotit-nhs-style-logo.svg?v=57",
   "/spotit-website-logo-v45.png",
   "/spotit-social-preview-v45.png",
