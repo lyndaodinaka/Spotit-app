9b81499612cdf124e8520f8cd7d23b8cc7502f8f
Lynda O <nwaekpelynda1994@gmail.com>
2026-07-16 22:28:28 +0100

    Convert navigation rail to Microsoft-style flyout

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,8 +1,8 @@
-const SPOTIT_CACHE = "spotit-shell-v55";
+const SPOTIT_CACHE = "spotit-shell-v56";
 const SHELL_ASSETS = [
   "/",
   "/index.html",
-  "/manifest.webmanifest?v=55",
+  "/manifest.webmanifest?v=56",
   "/assets/brand/spotit-app-logo-v39.png?v=41",
   "/spotit-website-logo-v45.png",
   "/spotit-social-preview-v45.png",
