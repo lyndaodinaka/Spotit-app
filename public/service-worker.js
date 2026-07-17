a2d9e88c95170623e3a57c19d9e61318080f4d89
Lynda O <nwaekpelynda1994@gmail.com>
2026-07-17 09:35:23 +0100

    Restore forgot password wording

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,8 +1,8 @@
-const SPOTIT_CACHE = "spotit-shell-v64";
+const SPOTIT_CACHE = "spotit-shell-v65";
 const SHELL_ASSETS = [
   "/",
   "/index.html",
-  "/manifest.webmanifest?v=64",
+  "/manifest.webmanifest?v=65",
   "/assets/brand/spotit-nhs-style-logo.svg?v=57",
   "/spotit-website-logo-v45.png",
   "/spotit-social-preview-v45.png",
